76a74d919d805453d034b0d0890c5ed4666d7e0b
Aaron Rodriguez <69766780+AaronERodriguez@users.noreply.github.com>
2026-07-22 14:49:12 -0600

    finalized base network

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,13 +3,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Network</title>
+
+    @vite('resources/css/app.css')
 </head>
 <body>
+    @if (session('success'))
+        <div id="flash" class="p-4 text-center bg-green-50 text-green-500 font-bold">
+            {{session('success')}}
+        </div>
+    @endif
+
     <header>
         <nav>
             <h1>Ninja Network</h1>
-            <a href="/ninjas">All Ninjas</a>
-            <a href="/ninjas/create">Create New Ninjas</a>
+            <a href="{{ route('ninjas.index')  }}">All Ninjas</a>
+            <a href="{{route('ninjas.create')}}">Create New Ninjas</a>
         </nav>
     </header>
 
--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,17 +1,18 @@
 <x-layout>
     <h2>Currently Available Ninjas</h2>
 
-    @if($greeting == 'hello')
-        <p>Hi from inside the if statement</p>
-    @endif
-
     <ul>
         @foreach($ninjas as $ninja)
             <li>
-                <x-card href="/ninjas/{{ $ninja['id'] }}" :highlight="$ninja['skill'] > 70">
-                    <h3>{{$ninja['name']}}</h3>
+                <x-card href="{{route('ninjas.show', $ninja->id)}}" :highlight="$ninja['skill'] > 70">
+                    <div>
+                        <h3>{{$ninja->name}}</h3>
+                        <p>{{$ninja->dojo->name}}</p>
+                    </div>
                 </x-card>
             </li>
         @endforeach
     </ul>
+
+    {{$ninjas->links()}}
 </x-layout>
